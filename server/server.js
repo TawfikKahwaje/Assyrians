c03f14b0d5fbcb6871985b7e97749ee9b49e49df
hussamEddinAlhendi <housam35@gmail.com>
2016-11-01 15:29:10 +0200

    Create git repo

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+
+
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+
+var port = process.env.PORT || 8000;
+
+app.listen(port);
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
